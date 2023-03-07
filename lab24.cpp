5428c27f5fed5c2edc9f86403efb0d0182df010a
werasak mayer <jokerwwe0542@gmail.com>
2023-03-07 16:08:12 +0700

    "lab24"

--- lab24.cpp
+++ lab24.cpp
@@ -24,10 +24,74 @@ ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
 	return ComplexNumber(real+c.real,imag+c.imag);
 }
 
+ComplexNumber operator+(double s,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
 ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
+ComplexNumber operator-(double s,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(((real)*(c.real))-((imag)*(c.imag)),((real)*(c.imag))+((c.real)*(imag)));
+}
+
+ComplexNumber operator*(double s,const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber((((real)*(c.real))+((imag)*(c.imag)))/(pow(c.real,2)+pow(c.imag,2)),(((c.real)*(imag))-((real)*(c.imag)))/((pow(c.real,2)+pow(c.imag,2))));
+}
+
+ComplexNumber operator/(double s,const ComplexNumber &c){
+	return ComplexNumber((((s)*(c.real))+((0)*(c.imag)))/(pow(c.real,2)+pow(c.imag,2)),(((c.real)*(0))-((s)*(c.imag)))/((pow(c.real,2)+pow(c.imag,2))));
+}
+
+
+bool operator==(double s,const ComplexNumber &c){
+	if(s == c.real && s == c.imag){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if( real== c.real && imag == c.imag){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+double ComplexNumber::abs(){
+	double a = pow(real,2);
+	double b = pow(imag,2);
+	double c = sqrt(a+b);
+	return c;
+}
+
+double ComplexNumber::angle(){
+	double a =atan2(imag,real)*57.2957795;
+	return a; 
+}
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+	if(c.imag <0){
+		if(c.real == 0)return os << c.imag << "i";
+		else return os << c.real << c.imag << "i";
+	}
+	if(c.imag == 0)return os << c.real;
+	if(c.imag > 0){
+		if(c.real == 0)return os << c.imag << "i";
+		else return os << c.real << "+" << c.imag << "i";
+	}
+	return os;
+}
 //Write your code here
 
 int main(){
